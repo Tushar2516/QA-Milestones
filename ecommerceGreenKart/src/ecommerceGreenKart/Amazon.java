9a14dbc91331304dd9fa138e6e7083bdf3f963a5
tushar4242 <pateltushar4242@gmail.com>
2023-05-12 09:55:46 -0400

    QA Ecommerece project commit by tushar

--- ecommerceGreenKart/src/ecommerceGreenKart/Amazon.java
+++ ecommerceGreenKart/src/ecommerceGreenKart/Amazon.java
@@ -1,19 +1,80 @@
 package ecommerceGreenKart;
 
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Amazon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//
-//		WebDriver driver = new ChromeDriver();
+
+		WebDriver driver = new ChromeDriver();
 //		driver.get("https://www.amazon.com/");
 //		Actions a = new Actions(driver);
-//		a.moveToElement(driver.findElement(css));
+//		WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+//		
+//		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+//		
+//		// move mouse
+//		a.moveToElement(move).contextClick().build().perform();
+//		
+		
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		driver.findElement(By.cssSelector(".blinkingText")).click();
+		Set<String> windows = driver.getWindowHandles();  // Switching from one to other Tab
+		Iterator<String>it = windows.iterator(); //Switching
+		String parentId = it.next();
+		String childId = it.next();
+		driver.switchTo().window(childId);
+		driver.findElement(By.cssSelector(".im-para.red")).getText();
+		String emailId = driver.findElement(By.cssSelector(".im-para.red")).getText().split("at")[1].trim().split(" ")[0];
+		System.out.println(emailId);
+		driver.switchTo().window(parentId);
+		driver.findElement(By.id("username")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("password")).sendKeys("learning");
+		driver.findElement(By.xpath("(//span[@class='checkmark'])[2]")).click();
+		
+		//Wait
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("okayBtn")));
+		driver.findElement(By.id("okayBtn")).click();
+		
+		WebElement options = driver.findElement(By.xpath("//select[@class = 'form-control']"));
+		Select dropdown = new Select(options);
+		dropdown.selectByValue("consult");
+		driver.findElement(By.id("terms")).click();
+		driver.findElement(By.id("signInBtn")).click();
+		
+		// Wait 
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Checkout")));
+
+		
+		//  List of products
+		List<WebElement> products = driver.findElements(By.cssSelector(".card-footer .btn-info"));
+		
+		// Iterating the product
+		for(int i = 0; i<products.size();i++) {
+			products.get(i).click();
+		}
+		
+		driver.findElement(By.partialLinkText("Checkout")).click();
+
+
 		
 	}
 
-}
+} 
